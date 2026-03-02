caacbd9ab3c10191b89f16c54e6315731307d7fa
isum-05 <isumh2005@gmail.com>
2026-03-02 22:22:35 +0530

    update

--- player.js
+++ player.js
@@ -5,13 +5,10 @@ const CANVAS_WIDTH = canvas.width = 200;
 const CANVAS_HEIGHT = canvas.height = 200;
 
 
-
 const playerImage = new Image();
-playerImage.src = 'image/sprite_sheet (1).png';
-const spriteWidth = 41;
-const spriteHeight = 43;
-let frameX = 0;
-let frameY = 0;
+playerImage.src = 'image/spriteSheet.png';
+const spriteWidth = 64;
+const spriteHeight = 64;
 let gameFrame = 0;
 const staggerFrames = 7;
 
@@ -19,9 +16,53 @@ const spriteAnimations = [];
 
 const animationStates = [
     { 
-        name:"idle",
-        frames: 7
-    }
+        name:"back",
+        frames:8
+    },
+
+    {
+        name:"back_left",
+        frames:6
+    },
+    {
+        name:"side_left",
+        frames:10
+    },
+
+    {
+        name:"front_left",
+        frames:8
+    },
+
+    {
+        name:"front",
+        frames:10
+    },
+
+    {
+        name:"front_right",
+        frames:8
+    },
+
+    {
+        name:"side_right",
+        frames:12
+    },
+
+    {
+        name:"back_right",
+        frames:6
+    },
+
+    {
+        name:"jump",
+        frames:15
+    },
+
+    {
+        name:"roll",
+        frames:6
+    },
 ];
 
 animationStates.forEach((state,index)=>{
@@ -33,15 +74,18 @@ animationStates.forEach((state,index)=>{
         let positionY = index * spriteHeight;
         frames.loc.push({x:positionX,y:positionY});
     }
-    spriteAnimations.push(frames);
+    spriteAnimations[state.name] = frames;
 });
+console.log(spriteAnimations);
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % 8;
-    frameX= spriteWidth * position;
-    ctx.drawImage(playerImage, frameX,frameY * spriteHeight,spriteWidth,
-    spriteHeight,0,0,spriteWidth,spriteHeight);
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations['front'].loc.length;
+    let frameX= spriteWidth * position;
+    let frameY = spriteAnimations['front'].loc[position].y;
+
+    ctx.drawImage(playerImage, frameX,frameY,spriteWidth,
+    spriteHeight,0,0,30,30);
 
     gameFrame++;
     requestAnimationFrame(animate);
